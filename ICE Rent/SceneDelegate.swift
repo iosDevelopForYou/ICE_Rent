f53cdae8e8f4bcab49fd794d2862ca2ea00e8649
iosDevelopForYou <iosdevelopforyou@gmail.com>
2023-07-03 17:52:51 +0400

    create some starting ui elements

--- ICE Rent/SceneDelegate.swift
+++ ICE Rent/SceneDelegate.swift
@@ -16,7 +16,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+              
+              // инициализируем наше окно
+              window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+              window?.windowScene = windowScene
+              
+              // инициализируем наш главный view controller
+              let viewController = ViewController()
+              let navigationController = UINavigationController(rootViewController: viewController)
+              
+              window?.rootViewController = navigationController
+              window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- ICE Rent/ViewController.swift
+++ ICE Rent/ViewController.swift
@@ -8,12 +8,77 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let mainBackground: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "mainBackground")
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
+    let titleBackView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.alpha = 0.6
+        view.layer.cornerRadius = 15
+        view.layer.shadowColor = UIColor.black.cgColor // Цвет тени
+        view.layer.shadowOpacity = 1 // Прозрачность тени (от 0 до 1)
+        view.layer.shadowOffset = CGSize(width: 10, height: 10) // Смещение тени
+        view.layer.shadowRadius = 5 // Радиус размытия тени
+        view.layer.shouldRasterize = true
+        view.layer.rasterizationScale = UIScreen.main.scale
+        return view
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "ICE Rent"
+        label.font = UIFont.boldSystemFont(ofSize: 28)
+        label.textColor = .black
+        label.sizeToFit()
+        return label
+    }()
+    
+    let subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "cервис бронирования льда"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .black
+        label.sizeToFit()
+        return label
+    }()
+    
+    func addViewLayout() {
+        view.addSubview(titleBackView)
+        view.addSubview(titleLabel)
+        view.addSubview(subTitleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleBackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            titleBackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleBackView.heightAnchor.constraint(equalToConstant: 78),
+            titleBackView.widthAnchor.constraint(equalToConstant: 258),
+            
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            subTitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5),
+            subTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.addSubview(mainBackground)
+        self.view.sendSubviewToBack(mainBackground)
+        
+        addViewLayout()
+        
     }
-
-
 }
 
